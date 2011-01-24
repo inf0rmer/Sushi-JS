79d053a33763cda122a095552e8d232b6ef83ad8
Bruno Abrantes <inf0rmer.realm@gmail.com>
2011-01-24 10:24:30 +0000

    first commit

---
+++ sushi.core.js
@@ -0,0 +1,19 @@
+/*!
+ * Sushi Core
+ * Copyright (C) 2011 Bruno Abrantes
+ * MIT Licensed
+ */
+
+/**
+ * Defining a private namespace
+ */
+(function( window ) {
+  
+	var Sushi = {
+		VERSION: '0.0.1'
+	};
+	
+	window.Sushi = (window.Sushi) ? window.Sushi : Sushi;
+    
+})( typeof window === 'undefined' ? this : window );
+
