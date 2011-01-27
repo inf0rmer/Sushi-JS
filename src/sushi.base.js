b6777ca71cfd4be13cfebbb28eb3fae954e56c0f
Bruno Abrantes <inf0rmer.realm@gmail.com>
2011-01-27 18:40:33 +0000

    Base now requires sushi.events
    Signed-off-by: Bruno Abrantes <inf0rmer.realm@gmail.com>

--- src/sushi.base.js
+++ src/sushi.base.js
@@ -7,6 +7,6 @@
 // Require library-wide dependencies, not in requirejs module format
 require([], function() {
 	// require Sushi modules in requirejs format
-	require(['sushi.core', 'sushi.utils'], function(){
+	require(['sushi.core', 'sushi.utils', 'sushi.events'], function(){
 	});
 });
