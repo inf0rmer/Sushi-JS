7fd68951257371111abc1ea61492b89bbfaed0b5
Bruno Abrantes <inf0rmer.realm@gmail.com>
2011-01-27 18:42:57 +0000

    Specified enumerable as a Sushi.utils module dependency
    Signed-off-by: Bruno Abrantes <inf0rmer.realm@gmail.com>

--- src/sushi.utils.js
+++ src/sushi.utils.js
@@ -4,7 +4,7 @@
  */
 define(
 	// Module dependencies
-	['sushi.utils.debug', 'sushi.utils.json'],
+	['sushi.utils.debug', 'sushi.utils.json', 'sushi.utils.enumerable'],
 	
 	/**
 	 * Sushi Utils
@@ -12,12 +12,30 @@ define(
 	 * @namespace Sushi
 	 * @class utils
 	 */
-	function(debug, json) {
+	function(debug, json, enumerable) {
 		var utilsNs = Sushi.namespace('utils'),
 			debugNs = Sushi.namespace('utils.debug'),
-			jsonNS = Sushi.namespace('utils.json');
+			jsonNs = Sushi.namespace('utils.json'),
+			enumerableNs = Sushi.namespace('utils.enumerable');
+			
 		
+		var utils = {
+		    /*
+		     *
+		     * @method isNumber
+		     * @param Value to test
+		     * 
+		     * @return {Boolean} Whether value is a number or not.
+		     */
+		    isNumber: function(number) {
+		        return (typeof number == "number");
+		    }
+		};
+		
+		Sushi.extend(Sushi.utils, utils);
+					
 		Sushi.extend(Sushi.utils.debug, debug);
 		Sushi.extend(Sushi.utils.json, json);
+		Sushi.extend(Sushi.utils.enumerable, enumerable);
 	}
 );
