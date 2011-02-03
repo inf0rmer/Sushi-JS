c344db88a3ce75ff38e479177af36dfa85a013f1
Bruno Abrantes <inf0rmer.realm@gmail.com>
2011-02-03 11:42:30 +0000

    Model constructor is almost finished, only stubs left are server methods.
    Signed-off-by: Bruno Abrantes <inf0rmer.realm@gmail.com>

--- src/sushi.Model.js
+++ src/sushi.Model.js
@@ -1,5 +1,5 @@
 define(
-	[],
+	['sushi.utils', 'sushi.utils.collection'],
 	
 	function() {
 		var Model = function(attributes, options) {
@@ -14,24 +14,51 @@ define(
 			this._previousAttributes = Sushi.extend(this.attributes);
 			this.init(attributes);
 			this.mid = Sushi.utils.uniqueId('m');
-		}
+		    this.topic = 'model_' + this.mid;
+		};
 		
-		Sushi.extend(Model.prototype, {
-			_previousAttributes: null,
+		Sushi.extend(Model.prototype, (function() {
+			var _previousAttributes = null,
 			
-			_changed: false,
+			_changed = false,
 			
-			initialize: function() {},
+			_trigger = function(evt, args) {
+			    if (!evt) {
+			        return false;
+			    }
+			    
+			    Sushi.event.publish(this.topic + ':' + evt, args);
+			    
+			    return this;
+			},
 			
-			copyAttributes: function() {
+			_performValidation = function(attrs, options) {
+			    var error = this.validate(attrs);
+			    
+			    if (error) {
+			        if (options.error) {
+			            options.error.call(this, error);
+			        } else {
+			            this._trigger('error', error);
+			        }
+			        
+			        return false;
+			    }
+			    
+			    return true;
+			},
+			
+			initialize = function() {},
+			
+			copyAttributes = function() {
 				return Sushi.extend({}, this.attributes);
 			},
 			
-			get: function(attr) {
+			get = function(attr) {
 				return this.attributes[attr];
 			},
 			
-			set: function(attrs, options) {
+			set = function(attrs, options) {
 				
 				options || (options = {});
 				
@@ -46,6 +73,9 @@ define(
 				var now = this.attributes;
 				
 				//Run validation...
+				if (!options.silent && this.validate && !this._performValidation(attrs, options)) {
+				    return false;
+				}
 				
 				// Check for changes of id
 				if ('id' in attrs) {
@@ -53,7 +83,7 @@ define(
 				}
 				
 				// Update the attributes
-				Sushi.each(attrs, function(attr) {
+				Sushi.utils.each(attrs, function(attr) {
 					var val = attrs[attr];
 
 					if (!Sushi.utils.isEqual(now[attr], val)) {
@@ -61,53 +91,109 @@ define(
 						
 						if (!options.silent) {
 							this._changed = true;
+						    this._trigger('change:' + attr, val);
 						}
 					}
 				});
 				
 				// Fire a change event in case model was changed
-				if (this._changed) {
-				    this.change()
+				if (!options.silent && this._changed) {
+    				this.change();
 				}
 				
 				return this;
 			},
 			
-			fetch: function() {
+			unset = function(attr, options) {
+			    options || (options = {});
+			    var value = this.attributes[attr],
+			        validObj = {};
+			        
+			    validObj[attr] = undefined;
+			    if (!options.silent && this.validate && !this._performValidation(validObj, options)) {
+			        return false;
+			    }			    
 			    
+			    // Remove the attribute
+			    delete this.attributes[attr];
+			    
+			    // Fire changed if options.silent was not set
+			    if (!options.silent) {
+			        this._changed = true;
+			        
+			        this._trigger('change:' + attr, undefined);
+			        this.change();
+			    }
+			    
+			    return this;
 			},
 			
-			save: function() {
+			clear = function(options) {
+			    options || (options = {});
 			    
+			    var old = this.attributes,
+			        validObj = {};
+			    
+			    Sushi.utils.each(old, function(attr){
+			        validObj[attr] = undefined;
+			    });
+			    		    
+			    if (!options.silent && this.validate && !this._performValidation(validObj, options)) {
+			        return false;
+			    }
+			    
+			    this.attributes = {};
+			    
+			    // Fire change event if options.silent was not set 
+			    if (!options.silent) {
+			        this._changed = true;
+			        Sushi.utils.each(old, function(attr){
+			            this._trigger('change:' + attr, undefined);
+			        });
+			        
+			        this.change();
+			    }
+			    
+			    return this;
 			},
 			
-			destroy: function() {
-			    
+			fetch = function() {
+			    return true;
 			},
 			
-			parse: function(resp) {
+			save = function() {
+			    return true;
+			},
+			
+			destroy = function() {
+			    return true;
+			},
+			
+			parse = function(resp) {
 			    return resp;
-			}
+			},
 			
-			change: function() {
-			    // TODO: fire change event
+			change = function() {
 			    this._previousAttributes = Sushi.extend(this.attributes);
 			    this._changed = false;
 			},
 			
-			hasChanged: function(attr) {
+			hasChanged = function(attr) {
 			    if (attr) {
-			        return (this._previousAttributes[attr] != this.attributes[attr]);
+			        if (this._previousAttributes[attr] != this.attributes[attr]) {
+			            return false;
+			        }
+			        
 			        return this._changed;
 			    }
 			},
 			
-			changedAttributes: function() {
+			changedAttributes = function() {
 			    var now = this.attributes,
 			    old = this._previousAttributes,
 			    changed = false;
 			    
-			    Sushi.each(now, function(attr){
+			    Sushi.utils.each(now, function(attr){
                     if (!Sushi.utils.isEqual(old[attr], now[attr])) {
                         changed = changed || {};
                         changed[attr] = now[attr];
@@ -117,18 +203,37 @@ define(
 			    return changed;
 			},
 			
-			previous: function(attr) {
+			previous = function(attr) {
 			    if (!attr || !this._previousAttributes) {
-			        return null
+			        return null;
 			    }
 			    
 			    return this._previousAttributes[attr];
 			},
 			
-			previousAttributes: function() {
+			previousAttributes = function() {
 			    return Sushi.extend(this._previousAttributes);
-			}
-		});
+			};
+			
+			return {
+			    initialize: initialize,
+			    copyAttributes: copyAttributes,
+			    get: get,
+			    set: set,
+			    unset: unset,
+			    clear: clear,
+			    fetch: fetch,
+			    save: save,
+			    destroy: destroy,
+			    parse: parse,
+			    change: change,
+			    hasChanged: hasChanged,
+			    changedAttributes: changedAttributes,
+			    previous: previous,
+			    previousAttributes: previousAttributes
+			};
+					
+		})());
 		
 		return Model;
 	}
