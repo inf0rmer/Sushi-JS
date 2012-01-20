adb5df9f940f28649c517b04140b3733f1634905
inf0rmer <inf0rmer.realm@gmail.com>
2012-01-20 14:33:11 +0000

    Added next and prev.

--- src/sushi.mvc.collection.js
+++ src/sushi.mvc.collection.js
@@ -141,6 +141,17 @@
 				return resp;
 			},
 			
+			next: function(model) {
+				var i = this.at(this.indexOf(model));
+				if (undefined === i || i < 0) return false;
+				return this.at(this.indexOf(model) + 1);
+			},
+			
+			prev: function(model) {
+				if (undefined === i || i < 1) return false;
+				return this.at(this.indexOf(model) - 1);
+			},
+			
 			_reset : function(options) {
 			  	this.length = 0;
 			  	this.models = [];
