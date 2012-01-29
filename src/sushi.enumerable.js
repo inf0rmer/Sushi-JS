751b588618b90df1188c8f876ff6cfe92b12670f
inf0rmer <inf0rmer.realm@gmail.com>
2012-01-29 15:13:25 +0000

    Simpler Enumerable extends the passed-in object with its methods for easier usage.

--- src/sushi.enumerable.js
+++ src/sushi.enumerable.js
@@ -6,6 +6,7 @@
  	// Module dependencies
  	[
  		'sushi.core',
+ 		'sushi.utils',
  		'sushi.utils.collection'
  	],
 
@@ -15,28 +16,22 @@
  	 * @namespace Sushi
  	 * @class Enumerable
  	 */
- 	function(Sushi, collection) {
+ 	function(Sushi, utils, collection) {
         Sushi.namespace('Enumerable', Sushi);
         
-        var Enumerable = new Sushi.Class({
-        	constructor: function(value) {
-        		this._val = value;
-        	},
-        	
-        	_val: null,
-        	
-        	get: function() {
-        		return this._val;
-        	}
-        })
-        , 	methods = ['each', 'map', 'reduce', 'reduceRight', 'find', 'detect',
+        var Enumerable = function(value) {
+        	Sushi.extend(value, Enumerable.prototype);
+        	return value;
+        };
+        
+        var	methods = ['each', 'map', 'reduce', 'reduceRight', 'find', 'detect', 'remove',
 			'filter', 'select', 'reject', 'every', 'all', 'some', 'any', 'include',
 			'contains', 'invoke', 'max', 'min', 'sortBy', 'sortedIndex', 'toArray', 'size',
 			'first', 'rest', 'last', 'without', 'indexOf', 'lastIndexOf', 'isEmpty', 'groupBy'];
 			
 		collection.each(methods, function(method) {
 			Enumerable.prototype[method] = function() {
-				return collection[method].apply(Sushi, [this._val].concat(collection.toArray(arguments)));
+				return collection[method].apply(Sushi, [this].concat(collection.toArray(arguments)));
 			};
 	  	});
 	  	
