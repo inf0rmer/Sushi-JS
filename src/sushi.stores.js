00162c23d734c0b80dec9f6e1626af2034e066f9
inf0rmer <inf0rmer.realm@gmail.com>
2012-01-29 18:40:26 +0000

    Narrowed down stores functionality to just register()

--- src/sushi.stores.js
+++ src/sushi.stores.js
@@ -6,10 +6,8 @@
  	// Module dependencies
  	[
  		'sushi.core',
- 		'sushi.utils',
  		'sushi.Enumerable',
- 		'sushi.error',
- 		'sushi.template'
+ 		'sushi.error'
  	],
 
  	/**
@@ -18,43 +16,16 @@
  	 * @namespace Sushi
  	 * @class stores
  	 */
- 	function(Sushi, utils, Enumerable, SushiError, template) {
+ 	function(Sushi, Enumerable, SushiError) {
         Sushi.namespace('stores', Sushi);
         
         var stores = new Enumerable([]);
         
         Sushi.extend(stores, {
-        	register: function(store) {
-        		if (typeof store !== 'Sushi.Store') {
-					throw new SushiError('Not a valid Sushi.Store.');
-					return false;
-				}
-				
-				if (utils.isUndefined(store.name) || utils.isEmpty(store.name)) {
-					throw new SushiError('Store must have a name.');
-					return false;
-				}
+        	register: function(store) {        						
+				if (utils.isUndefined(store.name) || utils.isEmpty(store.name)) throw new SushiError('Store must have a name.');
         		
         		this.push({id: store.name, store: store});
-        	},
-        	
-        	unregister: function(store) {
-        		var _toRemove = this.find(function(current) {
-        			return (current.name === store.name);
-        		});
-        		Sushi.log(this.indexOf(_toRemove));
-        		//this.remove({id: store.name, store: store});
-        	},
-        	
-        	syncAll: function() {
-        		this.each(function(store) {
-        			if (typeof store !== 'Sushi.Store') {
-        				throw new SushiError('Not a valid Sushi.Store.');
-        				return false;
-        			}
-        			
-        			return store.sync();        			
-        		});
         	}
         });
         
