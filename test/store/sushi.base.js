2109c5d7387e073015b17607127454ab006d892b
inf0rmer <inf0rmer.realm@gmail.com>
2012-01-30 17:02:52 +0000

    LocalStore tests.

---
+++ test/store/sushi.base.js
@@ -0,0 +1,18 @@
+/**
+ * Sushi JS
+ * Copyright (C) 2011 Bruno Abrantes
+ * MIT Licensed
+ *
+ * @namespace Sushi
+ * @class base
+ */
+// require Sushi modules in requirejs format
+require([
+	'sushi.mvc.model',
+	'sushi.mvc.collection',
+	'sushi.stores.LocalStore',
+	'sushi.stores.RemoteStore',
+	'sushi.Enumerable'
+	], 
+	function(){
+});
