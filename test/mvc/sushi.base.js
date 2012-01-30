99517d7874bf36ba3b745e67c84475027579e159
inf0rmer <inf0rmer.realm@gmail.com>
2012-01-30 10:56:51 +0000

    Organized tests into folders

---
+++ test/mvc/sushi.base.js
@@ -0,0 +1,18 @@
+/**
+ * Sushi JS
+ * Copyright (C) 2011 Bruno Abrantes
+ * MIT Licensed
+ *
+ * @namespace Sushi
+ * @class base
+ */
+// require Sushi modules in requirejs format
+require([
+	'sushi.mvc.model',
+	'sushi.mvc.view',
+	'sushi.mvc.collection',
+	'sushi.mvc.router',
+	'sushi.template'
+	], 
+	function(){
+});
