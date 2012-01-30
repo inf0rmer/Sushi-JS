4e245281781d0a96ddf7181b978f779977a66d75
inf0rmer <inf0rmer.realm@gmail.com>
2012-01-30 10:58:41 +0000

    Faster isEqual method
    Courtesy of kitcambridge:
    https://github.com/kitcambridge/underscore/commit/2a697e4bf528e2cc7f2634
    c0f879d795f00561f8

--- src/sushi.utils.js
+++ src/sushi.utils.js
@@ -4,7 +4,9 @@
  */
 define('sushi.utils',
 	// Module dependencies
-	['sushi.core'],
+	[
+		'sushi.core'
+	],
 	
 	/**
 	 * General purpose utility functions for the Sushi JS framework
@@ -12,7 +14,7 @@ define('sushi.utils',
 	 * @namespace Sushi
 	 * @class utils
 	 */
-	function() {
+	function(Sushi, collection) {
 		var _utilsNs = Sushi.namespace('utils'),
 		    _ArrayProto = Array.prototype,
 		    _FuncProto = Function.prototype,
@@ -184,53 +186,97 @@ define('sushi.utils',
 			 *
 			 * @return {Boolean} Whether objects are equal or not
 			 */
-			isEqual: function(a, b) {
-				// Check object identity.
-				if (a === b) { return true; }
+			isEqual: function(a, b, stack) {
+				if (this.isUndefined(stack)) stack = [];
+				// Identical objects are equal. `0 === -0`, but they aren't identical.
+				// See the Harmony `egal` proposal: http://wiki.ecmascript.org/doku.php?id=harmony:egal.
+				if (a === b) return a !== 0 || 1 / a == 1 / b;
+				// A strict comparison is necessary because `null == undefined`.
+				if (a == null || b == null) return a === b;
+				// Unwrap any wrapped objects.
+				if (a._chain) a = a._wrapped;
+				if (b._chain) b = b._wrapped;
+				// Invoke a custom `isEqual` method if one is provided.
+				if (this.isFunction(a.isEqual)) return a.isEqual(b);
+				if (this.isFunction(b.isEqual)) return b.isEqual(a);
+				// Compare `[[Class]]` names.
+				var className = toString.call(a);
+				if (className != toString.call(b)) return false;
+				switch (className) {
+				  	// Strings, numbers, dates, and booleans are compared by value.
+				  	case '[object String]':
+						// Primitives and their corresponding object wrappers are equivalent; thus, `"5"` is
+						// equivalent to `new String("5")`.
+						return String(a) == String(b);
+				  	case '[object Number]':
+						a = +a;
+						b = +b;
+						// `NaN`s are equivalent, but non-reflexive. An `egal` comparison is performed for
+						// other numeric values.
+						return a != a ? b != b : (a == 0 ? 1 / a == 1 / b : a == b);
+				  	case '[object Date]':
+				  	case '[object Boolean]':
+						// Coerce dates and booleans to numeric primitive values. Dates are compared by their
+						// millisecond representations. Note that invalid dates with millisecond representations
+						// of `NaN` are not equivalent.
+						return +a == +b;
+				  	// RegExps are compared by their source patterns and flags.
+				  	case '[object RegExp]':
+						return a.source == b.source &&
+						   a.global == b.global &&
+						   a.multiline == b.multiline &&
+						   a.ignoreCase == b.ignoreCase;
+				}
 				
-			    // Different types?
-			    var atype = typeof(a), 
-			    btype = typeof(b);
-			    if (atype != btype) { return false; }
-			    
-			    // Basic equality test (watch out for coercions).
-			    if (a == b) { return true; }
-			    
-			    // One is falsy and the other truthy.
-			    if ((!a && b) || (a && !b)) { return false; }
-			    
-			    // One of them implements an isEqual()?
-			    if (a.isEqual) { return a.isEqual(b); }
-			    
-			    // Check dates' integer values.
-			    if (this.isDate(a) && this.isDate(b)) { return ( a.getTime() === b.getTime() ); }
-			    
-			    // Both are NaN?
-			    if (this.isNaN(a) && this.isNaN(b)) { return false; }
-			    
-			    // Compare regular expressions.
-			    if (this.isRegExp(a) && this.isRegExp(b)) {
-			      return a.source     === b.source &&
-			             a.global     === b.global &&
-			             a.ignoreCase === b.ignoreCase &&
-			             a.multiline  === b.multiline;
-			    }
-			    
-			    // If a is not an object by this point, we can't handle it.
-			    if (atype !== 'object') { return false; }
-			    
-			    // Check for different array lengths before comparing contents.
-			    if (a.length && (a.length !== b.length)) { return false; }
-			    
-			    // Nothing else worked, deep compare the contents.
-			    var aKeys = this.keys(a), bKeys = this.keys(b);
-			    // Different object sizes?
-			    if (aKeys.length != bKeys.length) { return false; }
-			    // Recursive comparison of contents.
-			    for (var key in a) {
-			        if (!(key in b) || !this.isEqual(a[key], b[key])) { return false; }
-			    }
-			    return true;
+				if (typeof a != 'object' || typeof b != 'object') return false;
+				// Assume equality for cyclic structures. The algorithm for detecting cyclic
+				// structures is adapted from ES 5.1 section 15.12.3, abstract operation `JO`.
+				var length = stack.length;
+				while (length--) {
+				 	// Linear search. Performance is inversely proportional to the number of
+				  	// unique nested structures.
+				  	if (stack[length] == a) return true;
+				}
+				
+				// Add the first object to the stack of traversed objects.
+				stack.push(a);
+				var size = 0, result = true;
+				// Recursively compare objects and arrays.
+				if (className == '[object Array]') {
+				  	// Compare array lengths to determine if a deep comparison is necessary.
+				  	size = a.length;
+				  	result = size == b.length;
+				  	if (result) {
+						// Deep compare the contents, ignoring non-numeric properties.
+						while (size--) {
+					  		// Ensure commutative equality for sparse arrays.
+					  		if (!(result = size in a == size in b && this.isEqual(a[size], b[size], stack))) break;
+						}
+				  	}
+				} else {
+				  	// Objects with different constructors are not equivalent.
+				  	if ("constructor" in a != "constructor" in b || a.constructor != b.constructor) return false;
+				  	// Deep compare objects.
+				  	for (var key in a) {
+						if (hasOwnProperty.call(a, key)) {
+					  	// Count the expected number of properties.
+					  	size++;
+					  	// Deep compare each member.
+					  	if (!(result = hasOwnProperty.call(b, key) && this.isEqual(a[key], b[key], stack))) break;
+					}
+					}
+				
+					// Ensure that both objects contain the same number of properties.
+					if (result) {
+						for (key in b) {
+							if (hasOwnProperty.call(b, key) && !(size--)) break;
+						}
+						result = !size;
+					}
+				}
+				// Remove the first object from the stack of traversed objects.
+				stack.pop();
+				return result;
 			},
 			
 			/**
