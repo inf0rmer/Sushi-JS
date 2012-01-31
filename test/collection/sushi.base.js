15769f7d5decb7b153198ef2e2b78f07366ed1b6
inf0rmer <inf0rmer.realm@gmail.com>
2012-01-31 11:10:00 +0000

    Updated to new version of QUnit

--- test/collection/sushi.base.js
+++ test/collection/sushi.base.js
@@ -8,22 +8,8 @@
  */
 // require Sushi modules in requirejs format
 require([
-	'sushi.core', 
-	'sushi.utils', 
-	'sushi.utils.collection', 
-	'sushi.utils.debug', 
-	'sushi.utils.json', 
-	'sushi.utils.lang',
-	'sushi.mvc.model',
-	'sushi.mvc.view',
-	'sushi.mvc.collection',
-	'sushi.mvc.router',
-	'sushi.template',
-	'sushi.$',
-	'sushi.event',
-	'sushi.error',
-	'sushi.enumerable',
-	'sushi.Store.LocalStore'
+	'sushi.core',
+	'sushi.utils.collection'
 	], 
 	function(){
 });
