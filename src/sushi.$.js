70559379960659165bca656948235449b3b91366
inf0rmer <inf0rmer.realm@gmail.com>
2012-02-09 18:42:35 +0000

    Added further integration with Bonzo for several common use cases.

--- src/sushi.$.js
+++ src/sushi.$.js
@@ -96,6 +96,90 @@ define('sushi.$',
     		var bonzoed = Sushi.extend(bonzo(element), methods);
     		
     		return Sushi.extend(bonzoed, {
+    			
+    			parents: function (selector, closest) {
+					var collection = $(selector), j, k, p, r = []
+					for (j = 0, k = this.length; j < k; j++) {
+						p = this[j]
+						while (p = p.parentNode) {
+						  if (~indexOf(collection, p)) {
+							r.push(p)
+							if (closest) break;
+						  }
+						}
+					}
+					return $(uniq(r))
+				},
+			
+			  	parent: function() {
+					return $(uniq(bonzo(this).parent()))
+				},
+			
+			  	closest: function (selector) {
+					return this.parents(selector, true)
+				},
+			
+			  	first: function () {
+					return $(this.length ? this[0] : this)
+				},
+			
+			  	last: function () {
+					return $(this.length ? this[this.length - 1] : [])
+				},
+			
+			  	next: function () {
+					return $(bonzo(this).next())
+				},
+			
+			  	previous: function () {
+					return $(bonzo(this).previous())
+				},
+			
+			  	appendTo: function (t) {
+					return bonzo(this.selector).appendTo(t, this)
+				},
+			
+			  	prependTo: function (t) {
+					return bonzo(this.selector).prependTo(t, this)
+				},
+			
+			  	insertAfter: function (t) {
+					return bonzo(this.selector).insertAfter(t, this)
+				},
+			
+			  	insertBefore: function (t) {
+				  	return bonzo(this.selector).insertBefore(t, this)
+				},
+			
+				siblings: function () {
+				  	var i, l, p, r = []
+				  	for (i = 0, l = this.length; i < l; i++) {
+						p = this[i]
+						while (p = p.previousSibling) p.nodeType == 1 && r.push(p)
+						p = this[i]
+						while (p = p.nextSibling) p.nodeType == 1 && r.push(p)
+					}
+				  	return $(r)
+				},
+			
+			  	children: function () {
+				  	var i, el, r = []
+				  	for (i = 0, l = this.length; i < l; i++) {
+						if (!(el = bonzo.firstChild(this[i]))) continue;
+						r.push(el)
+						while (el = el.nextSibling) el.nodeType == 1 && r.push(el)
+				  	}
+				  	return $(uniq(r))
+				},
+			
+			  	height: function (v) {
+				  	return dimension(v, this, 'height')
+				},
+			
+			  	width: function (v) {
+				  	return dimension(v, this, 'width')
+				},
+    			
     			find: function (s) {
 					var r = [], i, l, j, k, els;
 					for (i = 0, l = this.length; i < l; i++) {
@@ -132,6 +216,36 @@ define('sushi.$',
 				}
     		});
     	};
+    	
+    	// Helpers
+    	function dimension(v, self, which) {
+			return v ?
+			self.css(which, v) :
+			function (r) {
+				if (!self[0]) return 0
+				r = parseInt(self.css(which), 10);
+				return isNaN(r) ? self[0]['offset' + which.replace(/^\w/, function (m) {return m.toUpperCase()})] : r
+			}()
+		}
+		
+		function indexOf(ar, val) {
+			for (var i = 0; i < ar.length; i++) if (ar[i] === val) return i
+			return -1
+	  	}
+	
+		function uniq(ar) {
+			var r = [], i = 0, j = 0, k, item, inIt
+			for (; item = ar[i]; ++i) {
+			  	inIt = false
+			  	for (k = 0; k < r.length; ++k) {
+					if (r[k] === item) {
+				  		inIt = true; break
+					}
+			  	}
+			  	if (!inIt) r[j++] = item
+			}
+			return r
+		}
     	
     	//Sugars
     	Sushi.fn = $;
