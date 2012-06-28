e58495e27465beb59bb88bb39d0da8ad43325f15
Bruno Abrantes <inf0rmer.realm@gmail.com>
2012-06-28 12:27:07 +0100

    Whitespace

--- src/sushi.mvc.collection.js
+++ src/sushi.mvc.collection.js
@@ -1,5 +1,5 @@
 /*
- * Sushi.Collection - 
+ * Sushi.Collection -
  *
  */
  define('sushi.mvc.collection',
@@ -22,7 +22,7 @@
  	 */
  	function(Sushi, event, utils, collection, Model, stores, SushiError) {
  		Sushi.namespace('Collection');
- 		
+
  		var Collection,
  			wrapError = function(onError, model, options) {
 				return function(resp) {
@@ -34,31 +34,31 @@
 				};
 		  	},
 		  	splice = Array.prototype.splice;
- 		
+
  		Collection = new Sushi.Class({
  			constructor: function(models, options) {
  				options || (options = {});
 				if (options.comparator) this.comparator = options.comparator;
 				this._reset();
-				if (models) this.reset(models, {silent: true});				
+				if (models) this.reset(models, {silent: true});
 				this.initialize.apply(this, arguments);
  			},
- 			
+
  			store: {},
- 			
+
  			model: Model,
- 			
+
  			initialize: function() {},
- 			
+
  			toJSON: function() {
  				return this.map(function(model){ return model.toJSON(); });
  			},
- 			
+
  			add: function(models, options) {
  				var i, index, length, model, cid, id, cids = {}, ids = {};
 				options || (options = {});
 				models = utils.isArray(models) ? models.slice() : [models];
-				
+
 				//Begin by turning bare objects into model references, and preventing invalid models or duplicate models from being added.
 				for (i = 0, length = models.length; i < length; i++) {
 					if (!(model = models[i] = this._prepareModel(models[i], options))) {
@@ -70,14 +70,14 @@
 					}
 					cids[cid] = ids[id] = model;
 			 	}
-			 	
+
 			 	//Listen to added models' events, and index models for lookup by id and by cid.
 			 	for (i = 0; i < length; i++) {
 					(model = models[i]).bind('all', this._onModelEvent, this);
 					this._byCid[model.cid] = model;
 					if (model.id != null) this._byId[model.id] = model;
 			  	}
-			  	
+
 			  	//Insert models into the collection, re-sorting if needed, and triggering add events unless silenced.
 			  	this.length += length;
 				index = options.at != null ? options.at : this.models.length;
@@ -91,7 +91,7 @@
 				}
 				return this;
 			},
-			
+
 			remove: function(models, options) {
 				var i, l, index, model;
 				options || (options = {});
@@ -110,10 +110,10 @@
 				}
 					this._removeReference(model);
 				}
-				
+
 				return this;
 			},
-			
+
 			push: function(model, options) {
 				model = this._prepareModel(model, options);
 				this.add(model, options);
@@ -137,20 +137,20 @@
 				this.remove(model, options);
 				return model;
 			},
-			
+
 			get: function(id) {
 				if (id == null) return null;
 			  	return this._byId[id.id != null ? id.id : id];
 			},
-			
+
 			getByCid: function(cid) {
 			  	return cid && this._byCid[cid.cid || cid];
 			},
-			
+
 			at: function(index) {
 			  	return this.models[index];
 			},
-			
+
 			where: function(attrs) {
 				if (utils.isEmpty(attrs)) return [];
 				return this.filter(function(model) {
@@ -160,26 +160,26 @@
 					return true;
 				});
 			},
-			
+
 			sort: function(options) {
 			  	options || (options = {});
 			  	if (!this.comparator) throw new SushiError('Cannot sort a set without a comparator');
-			  	
+
 			  	var boundComparator = utils.bind(this.comparator, this);
 				if (this.comparator.length == 1) {
 					this.models = this.sortBy(boundComparator);
 				} else {
 					this.models.sort(boundComparator);
 				}
-				
+
 				if (!options.silent) this.trigger('reset', this, options);
 				return this;
 			},
-			
+
 			pluck: function(attr) {
 			  	return Sushi.utils.map(this.models, function(model){ return model.get(attr); });
 			},
-			
+
 			reset: function(models, options) {
 			  	models  || (models = []);
 				options || (options = {});
@@ -191,23 +191,23 @@
 				if (!options.silent) this.trigger('reset', this, options);
 				return this;
 			},
-			
+
 			fetch: function(options) {
 			  	options = options ? collection.clone(options) : {};
       			if (options.parse === undefined) options.parse = true;
-			  	
+
 			  	var coll = this
 			  	, 	success = options.success;
-			  	
+
 			  	options.success = function(resp, status, xhr) {
 					coll[options.add ? 'add' : 'reset'](coll.parse(resp, xhr), options);
 					if (success) success(coll, resp);
 			  	};
-			  	
+
 			  	options.error = wrapError(options.error, coll, options);
 			  	return (this.sync || this.store.sync || stores.def.sync).call(this, 'read', this, options);
 			},
-			
+
 			create: function(model, options) {
 			  	var coll = this;
 				options = options ? utils.clone(options) : {};
@@ -226,29 +226,29 @@
 				model.save(null, options);
 				return model;
 			},
-			
+
 			parse: function(resp, xhr) {
 				return resp;
 			},
-			
+
 			next: function(model) {
 				var i = this.at(this.indexOf(model));
 				if (undefined === i || i < 0) return false;
 				return this.at(this.indexOf(model) + 1);
 			},
-			
+
 			prev: function(model) {
 				if (undefined === i || i < 1) return false;
 				return this.at(this.indexOf(model) - 1);
 			},
-			
+
 			_reset: function(options) {
 			  	this.length = 0;
 			  	this.models = [];
 			  	this._byId  = {};
 			  	this._byCid = {};
 			},
-			
+
 			_prepareModel: function(model, options) {
 			  	if (!(model instanceof Model)) {
 					var attrs = model;
@@ -260,14 +260,14 @@
 			  	}
 			  return model;
 			},
-			
+
 			_removeReference : function(model) {
 			  	if (this == model.collection) {
 					delete model.collection;
 			  	}
 			  	model.unbind('all', this._onModelEvent);
 			},
-			
+
 			_onModelEvent : function(ev, model, collection, options) {
 			  	if ((ev == 'add' || ev == 'remove') && collection != this) return;
 			  	if (ev == 'destroy') {
@@ -280,20 +280,20 @@
 				this.trigger.apply(this, arguments);
 			}
  		});
- 		
+
  		var methods = ['each', 'map', 'reduce', 'reduceRight', 'find', 'detect',
 			'filter', 'select', 'reject', 'every', 'all', 'some', 'any', 'include',
 			'contains', 'invoke', 'max', 'min', 'sortBy', 'sortedIndex', 'toArray', 'size',
 			'first', 'rest', 'last', 'without', 'indexOf', 'lastIndexOf', 'isEmpty', 'groupBy'];
-			
+
 		utils.each(methods, function(method) {
 			Collection.prototype[method] = function() {
 			  	return collection[method].apply(Sushi, [this.models].concat(collection.toArray(arguments)));
 			};
 	  	});
- 		
+
  		Sushi.extendClass(Collection, event);
- 		
+
  		Sushi.Collection = Collection;
  		return Collection;
  	}
