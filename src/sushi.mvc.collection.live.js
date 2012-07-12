6a7c4fcf76e671538c3ab7169d0728171a322d63
Bruno Abrantes <inf0rmer.realm@gmail.com>
2012-07-12 17:38:41 +0100

    Whitespace and security check

--- src/sushi.mvc.collection.live.js
+++ src/sushi.mvc.collection.live.js
@@ -19,7 +19,7 @@
  	 */
  	function(Sushi, utils, collectionUtils, Collection) {
 		Sushi.namespace('LiveCollection');
-        
+
 		// Define a Backbone Collection handling the details of running a "live"
 		// collection. Live collections are expected to handle fetching their own
 		// data, rather than being composed from separate models.
@@ -37,7 +37,7 @@
 			constructor: function(models, options) {
 				LiveCollection.Super.call(this, models, options);
 			},
-			
+
 			// A specialized fetch that can add new models and remove
 			// outdated ones. Models in response that are already in the
 			// collection are left alone. Usage:
@@ -45,14 +45,14 @@
 			//    myCollection.fetch({ diff: true });
 			fetch: function (options) {
 			  options = options || {};
-			  
+
 			  if (options.diff) {
 				var success = options.success,
 					prune = utils.bind(this.prune, this);
-			  
+
 				// Add new models, rather than resetting.
 				options.add = true;
-				
+
 				// Wrap the success callback, adding a pruning
 				// step after fetching.
 				options.success = function (collection, resp) {
@@ -60,27 +60,27 @@
 				  if (success) success(collection, resp);
 				};
 			  }
-			  
+
 			  // Delegate to original fetch method.
 			  LiveCollection.Super.prototype.fetch.call(this, options);
 			},
-			
+
 			// A custom add function that can prevent models with duplicate IDs
 			// from being added to the collection. Usage:
-			// 
+			//
 			//    myCollection.add({ unique: true });
 			add: function(models, options) {
 			  var modelsToAdd = models;
-			  
+
 			  // If a single model is passed in, convert it to an
 			  // array so we can use the same logic for both cases
 			  // below.
 			  if (!utils.isArray(models)) models = [models];
-			  
+
 			  options = Sushi.extend({
 				unique: true
 			  }, options);
-			  
+
 			  // If unique option is set, don't add duplicate IDs.
 			  if (options.unique) {
 				modelsToAdd = [];
@@ -90,10 +90,10 @@
 				  }
 				}, this);
 			  }
-			
+
 			  return LiveCollection.Super.prototype.add.call(this, modelsToAdd, options);
 			},
-			
+
 			// Weed out old models in collection, that are no longer being returned
 			// by the endpoint. Typically used as a callback for this.fetch's
 			// success option.
@@ -103,66 +103,67 @@
 			  var parsedResp = this.parse(resp),
 				  modelToID = function (model) { return model.id; },
 				  respIDs, collectionIDs, oldModels;
-			  
+
 			  // Convert array of JSON model objects to array of IDs.
 			  respIDs = collectionUtils.map(parsedResp, modelToID);
 			  collectionIDs = collectionUtils.map(collection.toJSON(), modelToID);
-			  
+
 			  // Find the difference between the two...
 			  oldModels = collectionUtils.difference(collectionIDs, respIDs);
-			  
+
 			  // ...and remove it from the collection
 			  // (remove can take IDs or objects).
 			  collection.remove(oldModels);
 			},
-			
+
 			// Poll this collection's endpoint.
 			// Options:
-			// 
+			//
 			// * `interval`: time between polls, in milliseconds.
 			// * `tries`: the maximum number of polls for this stream.
 			stream: function(options) {
 			  var polledCount = 0;
-			  
+
 			  // Cancel any potential previous stream.
 			  this.unstream();
-			  
+
 			  var update = utils.bind(function() {
 				// Make a shallow copy of the options object.
 				// `Backbone.collection.fetch` wraps the success function
-				// in an outer function (line `527`), replacing options.success. 
-				// That means if we don't copy the object every poll, we'll end 
+				// in an outer function (line `527`), replacing options.success.
+				// That means if we don't copy the object every poll, we'll end
 				// up modifying the reference object and creating callback inception.
-				// 
+				//
 				// Furthermore, since the sync success wrapper
 				// that wraps and replaces options.success has a different arguments
 				// order, you'll end up getting the wrong arguments.
 				var opts = collectionUtils.clone(options);
-				
+				opts = opts || {};
+
 				if (!opts.tries || polledCount < opts.tries) {
 				  polledCount = polledCount + 1;
-				  
+
 				  this.fetch(opts);
 				  this.pollTimeout = setTimeout(update, opts.interval || 1000);
 				}
 			  }, this);
-			
+
 			  update();
 			},
-			
+
 			// Stop polling.
 			unstream: function() {
 			  clearTimeout(this.pollTimeout);
 			  delete this.pollTimeout;
 			},
-			
+
 			isStreaming : function() {
 			  return utils.isUndefined(this.pollTimeout);
 			}
 		});
-		
+
 		Sushi.LiveCollection = LiveCollection;
-        
+
         return LiveCollection;
  	}
  );
