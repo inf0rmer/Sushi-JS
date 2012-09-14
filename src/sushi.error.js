7c95d0ac211e633ab57fa85c5bc685fe185b5e02
Bruno Abrantes <inf0rmer.realm@gmail.com>
2012-09-14 17:57:43 +0200

    Whitespace; Use Error.captureStackTrace conditionally.

--- src/sushi.error.js
+++ src/sushi.error.js
@@ -8,20 +8,21 @@ define('sushi.error',
 	[
 		'sushi.core'
 	],
-	
+
 	function(Sushi) {
 		function SushiError (msg) {
 			Error.call(this);
-			Error.captureStackTrace(this, arguments.callee);
+			if (Error.captureStackTrace)
+				Error.captureStackTrace(this, arguments.callee);
 			this.message = msg;
 			this.name = 'SushiError';
 		}
-		
+
 		SushiError.prototype.__proto__ = Error.prototype;
-		
+
 		Sushi.namespace('Error');
 		Sushi.Error = SushiError;
-		
+
 		return SushiError;
 	}
 );
